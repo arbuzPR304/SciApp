0dbff927f942dc1587935fb092f2220b66899999
Kasper Kalfas <k.kalfas93@gmail.com>
2016-11-19 09:38:02 +0100

    UI DONE

--- main/java/com/example/kaspero/sciapp2/MainActivity.java
+++ main/java/com/example/kaspero/sciapp2/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.kaspero.sciapp2;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity{
 
@@ -16,14 +17,28 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.sciAppTools);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.sciAppTools);
+        setSupportActionBar(toolbar);
+
+
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new MainFragment();
+            FragmentTransaction transaction = manager.beginTransaction();           /// WAY A
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO MAKING PHOTO
+
 
 
             }
---
+++ main/java/com/example/kaspero/sciapp2/MainFragment.java
@@ -0,0 +1,41 @@
+package com.example.kaspero.sciapp2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MainFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
