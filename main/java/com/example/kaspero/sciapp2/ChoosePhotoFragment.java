771d0e10be527f57e1e6bdcf8d1e254ee5892cb1
Kasper Kalfas <k.kalfas93@gmail.com>
2016-12-16 19:43:38 +0100

    Load photo done

--- main/java/com/example/kaspero/sciapp2/ChoosePhotoFragment.java
+++ main/java/com/example/kaspero/sciapp2/ChoosePhotoFragment.java
@@ -1,16 +1,13 @@
 package com.example.kaspero.sciapp2;
-
-
 import android.Manifest;
-import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -20,24 +17,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TabHost;
-
-import java.io.IOException;
-
-import static android.R.attr.button;
-import static android.R.attr.color;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import static android.app.Activity.RESULT_OK;
-import static com.example.kaspero.sciapp2.R.id.fab;
+import static android.bluetooth.BluetoothClass.Service.CAPTURE;
+
 
 public class ChoosePhotoFragment extends Fragment implements View.OnClickListener {
-
-    private static int RESULT_LOAD_IMAGE = 1;
+    // CONST VAR FOR TAKING PHOTO
+    private final int RESULT_LOAD_IMAGE = 1;
 
     private RelativeLayout imageContainer;
-    private Button buttonLoadPhoto;
+    private Button buttonLoadPhoto,buttonEditPhoto;
     private ImageView imageView;
+
+    private Uri contentURI;
 
 
 
@@ -49,7 +44,6 @@ public class ChoosePhotoFragment extends Fragment implements View.OnClickListene
     public static ChoosePhotoFragment newInstance() {
         ChoosePhotoFragment fragment = new ChoosePhotoFragment();
         Bundle args = new Bundle();
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -68,16 +62,17 @@ public class ChoosePhotoFragment extends Fragment implements View.OnClickListene
 
         buttonLoadPhoto = (Button)view.findViewById(R.id.chooseGalleryBtn);
         buttonLoadPhoto.setOnClickListener(this);
-
+        buttonEditPhoto = (Button)view.findViewById(R.id.editBtn);
+        buttonEditPhoto.setOnClickListener(this);
         imageView = (ImageView)view.findViewById(R.id.photoGaleryView);
-
         imageContainer = (RelativeLayout)view.findViewById(R.id.imagecontainer);
-
-
+        buttonEditPhoto.setEnabled(false);
 
         return view;
     }
 
+
+    // ON CLICK - FOR TAKE BITMAP FROM GALLERY
     @Override
     public void onClick(View v) {
         Button btn = (Button)v;
@@ -89,31 +84,56 @@ public class ChoosePhotoFragment extends Fragment implements View.OnClickListene
                 galleryIntent();
             }
         }
+        if(btn == buttonEditPhoto){
+            //TODO EDIT PHOTO
+        }
     }
 
+    // OPEN GALLERY INTENT
     private void galleryIntent()
     {
-
-//        Intent i = new Intent(
-//                Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//        startActivityForResult(i, RESULT_LOAD_IMAGE);
 
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
         startActivityForResult(Intent.createChooser(intent,
                 "Select Picture"), RESULT_LOAD_IMAGE);
-
     }
 
-
+    /**
+     * CAPTURE CHOOSEN PHOTO, NEXT TAKING URL, BITMAP IN
+     * TO MEMMORY AND THE AND SET IN IMAGEVIEW
+     * AND ENABLE EDIT BUTTON
+     * */
     public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         switch(requestCode) {
             case 1:
                 if(resultCode == RESULT_OK){
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    imageContainer.setBackgroundColor(Color.BLACK);
-                    imageView.setImageURI(selectedImage);
+                    if(imageReturnedIntent != null){
+                        contentURI = Uri.parse(imageReturnedIntent.getDataString());
+                        ContentResolver cr = getActivity().getContentResolver();
+                        InputStream in = null;
+                        try {
+                            in = cr.openInputStream(contentURI);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inSampleSize=4;
+                        Bitmap thumb = BitmapFactory.decodeStream(in,null,options);
+                        imageView.setImageBitmap(thumb);
+                        if (thumb != null) {
+                            buttonEditPhoto.setEnabled(true);
+                            final int sdk = android.os.Build.VERSION.SDK_INT;
+                            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                                buttonEditPhoto.setBackgroundDrawable( getResources().getDrawable(R.drawable.herro_button) );
+                            } else {
+                                buttonEditPhoto.setBackground( getResources().getDrawable(R.drawable.herro_button));
+                            }
+                        }
+                            imageContainer.setBackgroundColor(Color.BLACK);
+
+                    }
                 }
                 break;
         }
